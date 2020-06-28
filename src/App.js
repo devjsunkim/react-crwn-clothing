e8d6c26c2459caab2de36342b48d0a007cdfaadd
=Jinsun <devjsunkim@gmail.com>
2020-06-28 16:12:05 -0700

    : added cart-icon and cart dropdown components to app ,implemented cartReducer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.action'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
 
@@ -47,7 +47,7 @@ class App extends React.Component {
           <Route path='/shop' component={ShopPage}></Route>
           <Route exact path='/signin' render={() => 
           this.props.currentUser ? ( 
-            <Redirect to ='/' /> 
+            <Redirect to ='/'/> 
             ) : (
               <SignInAndSignUpPage/>
             )
